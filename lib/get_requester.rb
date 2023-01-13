0d1f060dc4d3307d7bf72f2d494bd9ad4c449a2d
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-01-13 14:29:32 -0800

    Completed Assignment

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,22 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+  attr_accessor :url
+
+  def initialize url
+    @url = url
+  end
+
+  def get_response_body
+    response = Net::HTTP.get_response(URI.parse(@url))
+    response.body
+  end
+
+  def parse_json
+    programs = JSON.parse(get_response_body)
+  end
+
+end
